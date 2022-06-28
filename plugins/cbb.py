2b9741b4c0282ec2c738db9aec962f7ef36fb496
D w i i <107328506+dwiiaksa@users.noreply.github.com>
2022-06-28 07:36:31 +0700

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -1,11 +1,10 @@
 # (©)Codexbotz
-# Recode by @mrismanaziz
-# t.me/SharingUserbot & t.me/Lunatic0de
+# Recode by @lucarlossx
 
 from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
 
 from bot import Bot
-from config import OWNER
+from config import CHANNEL, GROUP, OWNER
 
 
 @Bot.on_callback_query()
@@ -13,10 +12,10 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text=f"<b>Tentang Bot ini:\n\n@{client.username} adalah Bot Telegram untuk menyimpan Postingan atau File yang dapat Diakses melalui Link Khusus.\n\n • Creator: @{OWNER}\n • Source Code: <a href='https://github.com/putraicipiyey/File-Sharing'>File-Sharing</a>\n • Owner Repo: @fckandnight\n\n👨‍💻 Develoved by @dyecstar</b>\n",
+            text=f"<b>Tentang Bot ini:\n\n • Owner: @{OWNER}\n • Channel: @{CHANNEL}\n • Group: @{GROUP}\n • Source Code: <a href='https://t.me/L9BTX'>Klik Disini</a></b>\n",
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
-                [[InlineKeyboardButton("• ᴛᴜᴛᴜᴘ •", callback_data="close")]]
+                [[InlineKeyboardButton(" ᴛᴜᴛᴜᴘ ", callback_data="close")]]
             ),
         )
     elif data == "close":
